0d67c1ce5baca0822bf79a202a4e5338a0015eff
squarecat71522 <huliou880@gmail.com>
2024-01-10 01:11:24 +0800

    added log limit

--- modules/errorlog.py
+++ modules/errorlog.py
@@ -1,13 +1,21 @@
+from constants import ErrorlogConstant as Constant
 from resources import Path
 from threading import Thread
+from sys import exit
 from traceback import print_exception
 from datetime import datetime
 
-def log(error: BaseException) -> None:
-    def write():
-        with open(Path.ERRORLOG, "a") as file:
-            print(f"[{datetime.now()}]", file=file)
-            print_exception(error, file=file)
-            print(file=file)
-    Thread(target=write).start()
-        
+class Log:
+    __LOGS_LEFT = Constant.MAX_LOGS
+
+    @classmethod
+    def log(cls, error: BaseException) -> None:
+        def write():
+            with open(Path.ERRORLOG, "a") as file:
+                print(f"[{datetime.now()}]", file=file)
+                print_exception(error, file=file)
+                print(file=file)
+        if cls.__LOGS_LEFT == 0:
+            exit(1)
+        cls.__LOGS_LEFT -= 1
+        Thread(target=write).start()
