35c72ccf9fdab16685d30dcc8340e59bb1cc0009
squarecat71522 <huliou880@gmail.com>
2024-02-03 23:23:02 +0800

    v1.0

--- __main__.py
+++ __main__.py
@@ -1,9 +1,11 @@
 import pygame
 from sys import exit
-from modules import utils, language
+from modules.utils import FPSCounter
 from modules.resources import MAIN_SCREEN, CENTER_SCREEN, HIDDEN_SCREEN
+from modules.errorlog import Log
 from modules.display import CenterScreenDisplay
 from modules.interface import (
+    save, 
     GameInterface, 
     OptionInterface, 
     AchievementInterface, 
@@ -14,49 +16,73 @@ from modules.interface import (
     ControlRequest
 )
 
+def enter_event() -> pygame.event.Event:
+    return pygame.event.Event(pygame.MOUSEMOTION, pos=pygame.mouse.get_pos())
+
+def quit() -> None:
+    pygame.quit()
+    save()
+    OI.save()
+
 OI = OptionInterface()
 GI = GameInterface(OI.settings.language)
+AI = AchievementInterface(OI.settings.language)
 CI = ControlInterface(OI.settings.language)
-FPS = OI.settings.FPS
+FPS_SET = OI.settings.FPS
 CLOCK = pygame.time.Clock()
-Center_screen_dispaly = CenterScreenDisplay(CENTER_SCREEN)
-FPSCounter = utils.FPSCounter(50)
+CENTER_SCREEN_DISPLAY = CenterScreenDisplay(CENTER_SCREEN)
+FPS_COUNTER = FPSCounter(100)
 interface = GI
-
 while True:
-    CLOCK.tick(FPS)
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            exit()
-        interface.add_event(event)
-    for request in interface.get_request():
-        match request:
-            case (
-                GameRequest.QUIT | OptionRequest.QUIT 
-                | AchievementRequest.QUIT | ControlRequest.QUIT
-            ):
+    try:
+        FPS_COUNTER.append(CLOCK.tick(FPS_SET))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 exit()
-            case GameRequest.GOTO_OPTIONS:
-                interface = OI
-            case GameRequest.GOTO_CONTROLS:
-                interface = CI
-            case OptionRequest.CHANGE_LANGUAGE:
-                GI.language = OI.settings.language
-                CI.set_language(OI.settings.language)
-            case OptionRequest.CHANGE_FPS:
-                FPS = OI.settings.FPS
-            case OptionRequest.BACK:
-                interface = GI
-            case ControlRequest.BACK:
-                interface = GI
-    CENTER_SCREEN.fill((255, 255, 255))
-    FPSCounter.tick()
-    if interface.isGameInterface:
-        OI.display(HIDDEN_SCREEN, HIDDEN_SCREEN)
-        interface.display(MAIN_SCREEN, CENTER_SCREEN, FPS, FPSCounter.read())
-    else:
-        GI.display(HIDDEN_SCREEN, HIDDEN_SCREEN, FPS, FPSCounter.read())
-        interface.display(MAIN_SCREEN, CENTER_SCREEN)
-    Center_screen_dispaly.display(MAIN_SCREEN)
-    pygame.display.update()
+            interface.add_event(event)
+        for request in interface.get_request():
+            match request:
+                case (
+                    GameRequest.QUIT | OptionRequest.QUIT 
+                    | AchievementRequest.QUIT | ControlRequest.QUIT
+                ):
+                    exit()
+                case GameRequest.GOTO_OPTIONS:
+                    interface = OI
+                    interface.add_event(enter_event())
+                case GameRequest.GOTO_ACHIEVEMENT:
+                    interface = AI
+                    interface.add_event(enter_event())
+                case GameRequest.GOTO_CONTROLS:
+                    interface = CI
+                    interface.add_event(enter_event())
+                case GameRequest.RELOAD_ACHIEVEMENT_INTERFACE:
+                    AI.set_language(OI.settings.language)
+                case OptionRequest.CHANGE_LANGUAGE:
+                    GI.language = OI.settings.language
+                    AI.set_language(OI.settings.language)
+                    CI.set_language(OI.settings.language)
+                case OptionRequest.CHANGE_FPS:
+                    FPS_SET = OI.settings.FPS
+                case OptionRequest.BACK | AchievementRequest.BACK:
+                    interface = GI
+                    interface.add_event(enter_event())
+                case ControlRequest.BACK:
+                    interface = GI
+                    interface.add_event(enter_event())
+        if interface is GI:
+            GI.display(MAIN_SCREEN, CENTER_SCREEN, FPS_SET, FPS_COUNTER.read())
+        else:
+            GI.display(HIDDEN_SCREEN, HIDDEN_SCREEN, 0, 0)
+            interface.display(MAIN_SCREEN, CENTER_SCREEN)
+        CENTER_SCREEN_DISPLAY.display(MAIN_SCREEN)
+        pygame.display.update()
+    except (KeyboardInterrupt, SystemExit):
+        quit()
+        raise
+    except BaseException as e:
+        try:
+            Log.log(e)
+        except:
+            quit()
+            raise
